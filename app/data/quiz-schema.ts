e43ac09a7f48f9bf939f2f4ce47623cdf1c4863d
Dmitry Martynenko <dmitry@hirebrains.io>
2026-04-21 11:27:27 +0200

    Reuse shared states in quiz schema

--- app/data/quiz-schema.ts
+++ app/data/quiz-schema.ts
@@ -1,62 +1,10 @@
 import type {
   QuizQuestionDefinition,
-  QuizQuestionId,
-  QuizStateCode
+  QuizQuestionId
 } from '~/data/quiz-types'
+import { stateOptions } from '~/data/us-states'
 
-const solagreeStateOptions = [
-  ['AL', 'Alabama'],
-  ['AK', 'Alaska'],
-  ['AZ', 'Arizona'],
-  ['AR', 'Arkansas'],
-  ['CA', 'California'],
-  ['CO', 'Colorado'],
-  ['CT', 'Connecticut'],
-  ['DE', 'Delaware'],
-  ['DC', 'District of Columbia'],
-  ['FL', 'Florida'],
-  ['GA', 'Georgia'],
-  ['HI', 'Hawaii'],
-  ['ID', 'Idaho'],
-  ['IL', 'Illinois'],
-  ['IN', 'Indiana'],
-  ['IA', 'Iowa'],
-  ['KS', 'Kansas'],
-  ['KY', 'Kentucky'],
-  ['LA', 'Louisiana'],
-  ['ME', 'Maine'],
-  ['MD', 'Maryland'],
-  ['MA', 'Massachusetts'],
-  ['MI', 'Michigan'],
-  ['MN', 'Minnesota'],
-  ['MS', 'Mississippi'],
-  ['MO', 'Missouri'],
-  ['MT', 'Montana'],
-  ['NE', 'Nebraska'],
-  ['NV', 'Nevada'],
-  ['NH', 'New Hampshire'],
-  ['NJ', 'New Jersey'],
-  ['NM', 'New Mexico'],
-  ['NY', 'New York'],
-  ['NC', 'North Carolina'],
-  ['ND', 'North Dakota'],
-  ['OH', 'Ohio'],
-  ['OK', 'Oklahoma'],
-  ['OR', 'Oregon'],
-  ['PA', 'Pennsylvania'],
-  ['RI', 'Rhode Island'],
-  ['SC', 'South Carolina'],
-  ['SD', 'South Dakota'],
-  ['TN', 'Tennessee'],
-  ['TX', 'Texas'],
-  ['UT', 'Utah'],
-  ['VT', 'Vermont'],
-  ['VA', 'Virginia'],
-  ['WA', 'Washington'],
-  ['WV', 'West Virginia'],
-  ['WI', 'Wisconsin'],
-  ['WY', 'Wyoming']
-] as const satisfies ReadonlyArray<readonly [QuizStateCode, string]>
+const solagreeStateOptions = stateOptions.map(({ value, label }) => ({ id: value, label }))
 
 const solagreeQuizExplainerTitle = 'Why are we asking this?'
 
@@ -67,7 +15,7 @@ export const solagreeQuizQuestions = [
     title: 'Which state will your divorce be filed in?',
     description: 'Select the U.S. state or district connected to your divorce filing.',
     placeholder: 'Choose a state',
-    options: solagreeStateOptions.map(([id, label]) => ({ id, label }))
+    options: solagreeStateOptions
   },
   {
     id: 'children',
