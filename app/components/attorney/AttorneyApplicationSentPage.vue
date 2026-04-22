9cbfb606e4bf9274b653337bbc592725159a5597
Dmitry Martynenko <dmitry@hirebrains.io>
2026-04-22 12:30:03 +0200

    Add attorney application sent page

---
+++ app/components/attorney/AttorneyApplicationSentPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <main class="attorney-application-page">
+    <section
+      class="attorney-application-page__content attorney-application-page__content--sent"
+      aria-labelledby="attorney-application-sent-title"
+    >
+      <div class="attorney-application-sent">
+        <h1
+          id="attorney-application-sent-title"
+          class="attorney-application-sent__title"
+        >
+          Thank you for applying.
+        </h1>
+
+        <p class="attorney-application-sent__copy">
+          Our team will review your application and email you with next steps. We usually respond within 1-2 business days.
+        </p>
+      </div>
+    </section>
+
+    <SiteFooter />
+  </main>
+</template>
---
+++ app/pages/attorney-application/sent.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import AttorneyApplicationSentPage from '~/components/attorney/AttorneyApplicationSentPage.vue'
+
+useSeoMeta({
+  title: 'Application Received | Solagree',
+  description: 'Your Solagree attorney partner application has been received.',
+  robots: 'noindex, nofollow'
+})
+</script>
+
+<template>
+  <AttorneyApplicationSentPage />
+</template>
