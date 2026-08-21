5e3138f0f927f6b7f258bb7c6b2e44a237804317
Dmitry Martynenko <dmitry@hirebrains.io>
2026-08-21 12:52:14 +0200

    feat(news): add approved media coverage

---
+++ app/pages/news.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NewsIndexPage from '~/components/news/NewsIndexPage.vue'
+import { publishedNewsItems } from '~/data/resource-content'
+
+useSolagreeSeo({
+  title: 'News & Press',
+  description: 'Official announcements and media coverage about Solagree’s approach to divorce resolution.',
+  path: '/news'
+})
+</script>
+
+<template>
+  <NewsIndexPage :items="publishedNewsItems" />
+</template>
